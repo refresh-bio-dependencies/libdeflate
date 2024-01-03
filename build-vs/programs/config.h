99e83ebfbc9c8b2ceebab389939df4385e6e8ae7
marekkokot <marek.koki@gmail.com>
2024-01-03 17:35:13 +0100

    cmake free solution

---
+++ build-vs/programs/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Is the clock_gettime() function available? */
+/* #undef HAVE_CLOCK_GETTIME */
+
+/* Is the futimens() function available? */
+/* #undef HAVE_FUTIMENS */
+
+/* Is the posix_fadvise() function available? */
+/* #undef HAVE_POSIX_FADVISE */
+
+/* Is the posix_madvise() function available? */
+/* #undef HAVE_POSIX_MADVISE */
+
+/* Does stat() provide nanosecond-precision timestamps? */
+/* #undef HAVE_STAT_NANOSECOND_PRECISION */
+
+#endif /* CONFIG_H */
